b79728d0e008d4f2dc23534478aa42aff0e4cb9e
aboroch <aboroch@us.ibm.com>
2017-11-20 11:35:03 -0500

    getting the post to work

--- app/server.js
+++ app/server.js
@@ -15,7 +15,7 @@ var friends = require("./data/friends.js");
 // })
 
 app.post("/api/friends", function(req, res) {
-  
+
   var surveyResults = req.body;
   console.log(surveyResults);
   surveyResults.name = surveyResults.name.replace(/\s+/g, "").toLowerCase();
@@ -23,11 +23,14 @@ app.post("/api/friends", function(req, res) {
   surveyResults.photo = surveyResults.photo.replace(/\s+/g, "").toLowerCase();
   console.log(surveyResults.photo);
   // surveyResults.scores = parseInt(surveyResults.scores[0].replace(/\s+/g, ""));
-  console.log(surveyResults.scores[0]);
-  var newFriend = res.json(surveyResults);
-  for(var i = 0; i < newFriend.scores.length; i++) {
-    newFriend.scores[i] = parseInt(newFriend.scores[i].replace(/\s+/g, ""));
-  }
+  console.log(surveyResults['scores[]'][0]);
+
+  res.json(surveyResults);
+  // var newFriend = res.json(surveyResults);
+  // console.log('new', newFriend);
+  // for(var i = 0; i < newFriend['scores[]'].length; i++) {
+  //   newFriend.scores[i] = parseInt(newFriend.scores[i].replace(/\s+/g, ""));
+  // }
   // console.log("Scores", surveyResults.scores);
   // friends.push(surveyResults);
   // console.log("friends array", friends);
@@ -57,17 +60,17 @@ app.post("/api/friends", function(req, res) {
 
 // // Determine the user's most compatible friend using the following as a guide:
 // // Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
-// // With that done, compare the difference between current user's scores against 
-// those from other users, question by question. 
+// // With that done, compare the difference between current user's scores against
+// those from other users, question by question.
 // Add up the differences to calculate the  totalDifference.
 // // Example:
 // // User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
 // // User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
 // // Total Difference: 2 + 1 + 2 = 5
-// // Remember to use the absolute value of the differences. Put another way: 
+// // Remember to use the absolute value of the differences. Put another way:
 // no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on.
 // // The closest match will be the user with the least amount of difference.
-// // Once you've found the current user's most compatible friend, display the 
+// // Once you've found the current user's most compatible friend, display the
 // result as a modal pop-up.
 // // The modal should display both the name and picture of the closest match.
 
@@ -75,4 +78,3 @@ app.post("/api/friends", function(req, res) {
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 })
-
